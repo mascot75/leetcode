51fdb3ba20947ca77a380ca421c45e80c01fc881
mascot75 <liuzhanjie75@gmail.com>
2019-04-18 21:46:17 +0800

    48. 旋转图像 删除部分不必要代码

--- src/test/java/medium/RotateTest.java
+++ src/test/java/medium/RotateTest.java
@@ -18,9 +18,6 @@ public class RotateTest {
     public void rotate() {
         int[][] array = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         rotate.rotate(array);
-        for (int[] ints : array) {
-            System.out.println(Arrays.toString(ints));
-        }
         assertArrayEquals(new int[][]{{7, 4, 1}, {8, 5, 2}, {9, 6, 3}}, array);
     }
 
