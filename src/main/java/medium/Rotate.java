734fd44730290c1bb6a7b3b0ab886d73452e3bd4
mascot75 <liuzhanjie75@gmail.com>
2019-04-18 21:36:55 +0800

    48. 旋转图像

---
+++ src/main/java/medium/Rotate.java
@@ -0,0 +1,35 @@
+package medium;
+
+/**
+ * 给定一个 n × n 的二维矩阵表示一个图像。
+ *
+ * 将图像顺时针旋转 90 度。
+ *
+ * 说明：
+ *
+ * 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像
+ * @author mascot
+ * @date 2019/4/18 19:48
+ */
+public class Rotate {
+    public void rotate(int[][] matrix) {
+        rotate(matrix, 0);
+    }
+
+    private void rotate(int[][] matrix, int index) {
+        int rotate = (matrix.length & 1) == 0 ? matrix.length >> 1 : matrix.length >> 1;
+        if (rotate <= index) {
+            return;
+        }
+
+        for (int i = index; i < matrix.length - 1 - index; i++) {
+            int temp = matrix[index][i];
+            matrix[index][i] = matrix[matrix.length - i - 1][index];
+            matrix[matrix.length - i - 1][index] = matrix[matrix.length - index - 1][matrix.length - i - 1];
+            matrix[matrix.length - index - 1][matrix.length - i - 1] = matrix[i][matrix.length - index - 1];
+            matrix[i][matrix.length - index - 1] = temp;
+        }
+
+        rotate(matrix, index + 1);
+    }
+}
---
+++ src/test/java/medium/RotateTest.java
@@ -0,0 +1,33 @@
+package medium;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author mascot
+ * @date 2019/4/18 19:49
+ */
+public class RotateTest {
+
+    private Rotate rotate = new Rotate();
+
+    @Test
+    public void rotate() {
+        int[][] array = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate.rotate(array);
+        for (int[] ints : array) {
+            System.out.println(Arrays.toString(ints));
+        }
+        assertArrayEquals(new int[][]{{7, 4, 1}, {8, 5, 2}, {9, 6, 3}}, array);
+    }
+
+    @Test
+    public void rotate2() {
+        int[][] array = new int[][]{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        rotate.rotate(array);
+        assertArrayEquals(new int[][]{{15, 13, 2, 5}, {14, 3, 4, 1}, {12, 6, 8, 9}, {16, 7, 10, 11}}, array);
+    }
+}
