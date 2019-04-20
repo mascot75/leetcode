79a85b77ce782b2210852049efc3c16d654e6cc1
mascot75 <liuzhanjie75@gmail.com>
2019-04-20 16:26:44 +0800

    189. 旋转数组 更新算法，使用了翻转数组的方法

--- src/main/java/easy/RotateArray.java
+++ src/main/java/easy/RotateArray.java
@@ -10,10 +10,20 @@ public class RotateArray {
     public void rotate(int[] nums, int k) {
 
         k = k % nums.length;
+        reverse(nums, 0, nums.length -1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
 
-        int[] array = new int[k];
-        System.arraycopy(nums, nums.length - k , array, 0, k);
-        System.arraycopy(nums, 0, nums, k, nums.length - k);
-        System.arraycopy(array, 0, nums, 0, k);
+    private void reverse(int[] nums, int begin, int end) {
+        while (begin < end) {
+            swap(nums, begin++, end--);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
