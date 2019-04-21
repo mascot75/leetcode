057fa2ea741fcf28a5f79ae553086d6fc4ad6e03
mascot75 <liuzhanjie75@gmail.com>
2019-04-21 14:32:11 +0800

    62. 不同路径 优化代码，减少空间复杂度

--- src/main/java/medium/UniquePaths.java
+++ src/main/java/medium/UniquePaths.java
@@ -14,29 +14,14 @@ public class UniquePaths {
 
     public int uniquePaths(int m, int n) {
 
-        if (n <= 0 || m <= 0) {
-            return 0;
-        }
-
-        if (n == 1 || m == 1) {
-            return 1;
-        }
-
-        int[][] paths = new int[m][n];
+        int[] paths = new int[n];
+        paths[0] = 1;
         for (int i = 0; i < m; i++) {
-            paths[i][0] = 1;
-        }
-
-        for (int i = 0; i < n; i++) {
-            paths[0][i] = 1;
-        }
-
-        for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                paths[i][j] = paths[i - 1][j] + paths[i][j - 1];
+                paths[j] += paths[j - 1];
             }
         }
 
-        return paths[m - 1][n - 1];
+        return paths[n - 1];
     }
 }
