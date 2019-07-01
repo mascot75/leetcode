a7d676d210919177766fa9c478b98405c8212ee4
mascot75 <liuzhanjie75@gmail.com>
2019-07-01 19:56:59 +0800

    60. 第k个排列

---
+++ src/main/java/medium/PermutationSequence.java
@@ -0,0 +1,49 @@
+package medium;
+
+/**
+ * 给出集合 [1,2,3,…,n]，其所有元素共有 n! 种排列。
+ *
+ * 按大小顺序列出所有排列情况，并一一标记，当 n = 3 时, 所有排列如下：
+ *
+ * "123"
+ * "132"
+ * "213"
+ * "231"
+ * "312"
+ * "321"
+ * 给定 n 和 k，返回第 k 个排列。
+ *
+ * 说明：
+ *
+ * 给定 n 的范围是 [1, 9]。
+ * 给定 k 的范围是[1,  n!]。
+ *
+ * @author mascot
+ * @date 2019/7/1 19:22
+ */
+public class PermutationSequence {
+    /**
+     * 康拓展开
+     * @param n 给定的范围
+     * @param k 第 K 个数
+     * @return 第 k 全排列
+     */
+    public String getPermutation(int n, int k) {
+
+        int[] factorials = new int[n];
+        factorials[0] = 1;
+        for (int i = 1; i < factorials.length; i++) {
+            factorials[i] = factorials[i - 1] * i;
+        }
+
+        StringBuilder sequence = new StringBuilder("123456789");
+        StringBuilder result = new StringBuilder();
+
+        for (--k; --n >= 0; k %= factorials[n]) {
+            int i = k / factorials[n];
+            result.append(sequence.charAt(i));
+            sequence.deleteCharAt(i);
+        }
+        return result.toString();
+    }
+}
---
+++ src/test/java/medium/PermutationSequenceTest.java
@@ -0,0 +1,20 @@
+package medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author mascot
+ * @date 2019/7/1 19:23
+ */
+public class PermutationSequenceTest {
+
+    private PermutationSequence sequence = new PermutationSequence();
+
+    @Test
+    public void getPermutation() {
+        assertEquals("213", sequence.getPermutation(3, 3));
+        assertEquals("2314", sequence.getPermutation(4, 9));
+    }
+}
