a1abd8772b18fb5d0dec26b7f2e2525827a7d621
mascot75 <liuzhanjie75@gmail.com>
2019-07-15 21:48:56 +0800

    141. 环形链表

---
+++ src/main/java/easy/LinkedListCycle.java
@@ -0,0 +1,81 @@
+package easy;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * 141. 环形链表
+ *
+ * 给定一个链表，判断链表中是否有环。
+ *
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ *
+ * @author mascot
+ * @date 2019/7/15 21:35
+ */
+public class LinkedListCycle {
+    public boolean hasCycle2(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        Set<ListNode> nodes = new HashSet<>();
+        ListNode node = head;
+        while (node != null) {
+            if (nodes.contains(node)) {
+                return true;
+            } else {
+                nodes.add(node);
+                node = node.next;
+            }
+        }
+        return false;
+    }
+}
+
+class ListNode {
+    private int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ListNode node = (ListNode) o;
+        return val == node.val &&
+                Objects.equals(next, node.next);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, next);
+    }
+}
