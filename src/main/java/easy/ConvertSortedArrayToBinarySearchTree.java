322c18af470828b6ade326ece25724aa53469b08
mascot75 <liuzhanjie75@gmail.com>
2019-07-26 19:23:28 +0800

    108. 将有序数组转换为二叉搜索树

---
+++ src/main/java/easy/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,30 @@
+package easy;
+
+/**
+ * 108. 将有序数组转换为二叉搜索树
+ *
+ * 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+ *
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+ *
+ * @author mascot
+ * @date 2019/7/26 19:16
+ */
+public class ConvertSortedArrayToBinarySearchTree {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return nums == null ? null : buildTree(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildTree(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int mid = (start + end) >> 1;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = buildTree(nums, start, mid - 1);
+        node.right = buildTree(nums, mid + 1, end);
+        return node;
+    }
+
+}
